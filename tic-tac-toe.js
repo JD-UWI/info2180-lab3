1d6518840ea25c745a35c34fc64800458e7b31df
Jordan Dwyer <dwyer.jordan9@gmail.com>
2022-10-11 21:58:41 -0500

    Completed exercise 1

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -8,11 +8,23 @@ window.onload = function(){
     var playerO = "O";
     var wincon = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
-
     divsquare.forEach((element) =>{
         element.classList.add("square");
         element.innerHTML = "";
 
+        element.addEventListener("click", function(){
+            if ((cplayer == true) && (element.innerHTML == "")){
+                element.innerHTML = playerX;
+                element.classList.add("X");
+                cplayer = false;
+            }
+            else if ((cplayer == false) && (element.innerHTML =="")){
+                element.innerHTML = playerO;
+                element.classList.add("O");
+                cplayer = true;
+            }
+
+        });
 
     });
 }
